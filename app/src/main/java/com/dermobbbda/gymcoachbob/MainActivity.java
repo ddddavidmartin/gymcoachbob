54832ff70a14285e5095a825351d88a7f5f327fc
David Martin <davidm@breakawayconsulting.com.au>
2015-02-22 14:47:38 +1100

    Extend app with message handling activity.
    Now the message we send is displayed in a separate activity.

--- app/src/main/java/com/dermobbbda/gymcoachbob/MainActivity.java
+++ app/src/main/java/com/dermobbbda/gymcoachbob/MainActivity.java
@@ -1,12 +1,16 @@
 package com.dermobbbda.gymcoachbob;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.dermobbbda.gymcoachbob.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,16 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        // Handle the sending of a message.
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
     }
 }
