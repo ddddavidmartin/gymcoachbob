b126a1c5a12e8dd847287e89e33ba5187224a7bc
David Martin <david.martin.mailbox@gmail.com>
2016-01-27 22:32:45 +1100

    Add button to add new workout sessions.
    It only prints a log message for now, but we will use it to add Sessions
    from the Exercise view.

--- app/src/main/java/com/dermobbbda/gymcoachbob/ViewExerciseActivity.java
+++ app/src/main/java/com/dermobbbda/gymcoachbob/ViewExerciseActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
 public class ViewExerciseActivity extends Activity {
+    private static final String TAG = "GCB";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -50,5 +54,26 @@ public class ViewExerciseActivity extends Activity {
 
         mAdapter = new SessionViewAdapter(mSessions);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.view_exercise_activity_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_new_session:
+                Log.d(TAG, "New Session button pressed");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
