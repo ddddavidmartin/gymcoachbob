58ef3ac61f232377601c1dff519c7eaccba84485
David Martin <david.martin.mailbox@gmail.com>
2016-01-30 23:15:42 +1100

    Add basic date picker button and dialog for NewSessionActivity.
    So far this is just a basic button to select the date, and nothing fancy
    about it. But we can use it as a start to flesh out a better design.
    
    It still needs to be integrated into the actual creation of the Session
    as well.

--- app/src/main/java/com/dermobbbda/gymcoachbob/NewSessionActivity.java
+++ app/src/main/java/com/dermobbbda/gymcoachbob/NewSessionActivity.java
@@ -1,12 +1,47 @@
 package com.dermobbbda.gymcoachbob;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class NewSessionActivity extends Activity {
+    private static final String TAG = "GCB";
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Log.d(TAG, "Picked date: " + year + "/" + month + "/" + day);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_session);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 }
