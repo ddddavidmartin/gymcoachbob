62e1bc43e893c74501fed04345bae03c27863bcf
David Martin <david_martin@fastmail.com>
2016-06-15 18:05:03 +1000

    Cosmetics: Remove superfluous blank line from NewExerciseActivity.java.

--- app/src/main/java/com/dermobbbda/gymcoachbob/NewExerciseActivity.java
+++ app/src/main/java/com/dermobbbda/gymcoachbob/NewExerciseActivity.java
@@ -23,7 +23,6 @@ public class NewExerciseActivity extends ActionBarActivity {
         setContentView(R.layout.activity_new_exercise);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
