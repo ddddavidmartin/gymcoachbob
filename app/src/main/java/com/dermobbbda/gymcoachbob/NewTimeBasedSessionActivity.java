cf8261714642f56a63c2765bfbaf28df8d76620c
David Martin <david_martin@fastmail.com>
2016-06-28 17:30:34 +1000

    Initialise time and distance NumberPickers for new time-based Sessions.
    This is a first step towards being able to add new time-based Sessions.
    For now we use a very basic layout and just have two NumberPickers to
    select time and distance. For now values can be selected but it is not
    possible yet to actually create Sessions.
    
    But, we can work on this as a baseline. Ideally we will have the ability
    to properly pick times (with hours/minutes/seconds) and distances with
    decimal parts.

--- app/src/main/java/com/dermobbbda/gymcoachbob/NewTimeBasedSessionActivity.java
+++ app/src/main/java/com/dermobbbda/gymcoachbob/NewTimeBasedSessionActivity.java
@@ -8,8 +8,11 @@ package com.dermobbbda.gymcoachbob;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.NumberPicker;
 
 public class NewTimeBasedSessionActivity extends NewExerciseSessionActivity {
+    private int mTime;
+    private double mDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,32 @@ public class NewTimeBasedSessionActivity extends NewExerciseSessionActivity {
         setContentView(R.layout.activity_new_timebased_session);
 
         initialiseDateText();
+
+        NumberPicker timePicker = (NumberPicker) findViewById(R.id.new_session_time_picker);
+        timePicker.setMaxValue(getResources().getInteger(R.integer.new_session_max_time));
+        timePicker.setMinValue(getResources().getInteger(R.integer.new_session_min_time));
+        timePicker.setValue(getResources().getInteger(R.integer.new_session_min_time));
+        /* Allow the picker to wrap around as there is quite a large number of possible values. */
+        timePicker.setWrapSelectorWheel(true);
+        timePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+                mTime = newValue;
+            }
+        });
+
+        NumberPicker distancePicker = (NumberPicker) findViewById(R.id.new_session_distance_picker);
+        distancePicker.setMaxValue(getResources().getInteger(R.integer.new_session_max_distance));
+        distancePicker.setMinValue(getResources().getInteger(R.integer.new_session_min_distance));
+        distancePicker.setValue(getResources().getInteger(R.integer.new_session_min_distance));
+        /* Allow the picker to wrap around as there is quite a large number of possible values. */
+        distancePicker.setWrapSelectorWheel(true);
+        distancePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+                mDistance = newValue;
+            }
+        });
     }
 
     public void addSession(View view) {
