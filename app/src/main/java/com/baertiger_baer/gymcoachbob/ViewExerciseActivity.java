2558aa4c5dd2ab1579612ab02f03141be46b398d
David Martin <david_martin@fastmail.com>
2017-01-29 15:47:03 +1100

    Rename view_exercise.xml to view_weightbased_exercise.xml.
    Before it did not matter as the weight-based exercises were all we had.
    Now that we are introducing the time-based exercises we need to
    distinguish between the two.

--- app/src/main/java/com/baertiger_baer/gymcoachbob/ViewExerciseActivity.java
+++ app/src/main/java/com/baertiger_baer/gymcoachbob/ViewExerciseActivity.java
@@ -58,7 +58,7 @@ public class ViewExerciseActivity extends ActionBarActivity {
 
     /** Set up the Exercise layout for when there are Sessions for this Exercise. */
     protected void setUpNonEmptyExercise() {
-        setContentView(R.layout.activity_view_exercise);
+        setContentView(R.layout.activity_view_weightbased_exercise);
         mRecyclerView = (RecyclerView) findViewById(R.id.view_exercise_recyclerview);
         /* Improves performance. Only set to true if changes in content do not change the
          * layout size of the RecyclerView. */
