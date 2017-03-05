bd60b125a755c7ce9645137e578536a7f29f6671
David Martin <david_martin@fastmail.com>
2017-03-05 23:40:14 +1100

    Remove superfluous inherited onOptionsItemSelected method.
    We may have done something special in there at some point, but now we
    are just calling the super method and may as well not override it at
    all.

--- app/src/main/java/com/baertiger_baer/gymcoachbob/NewExerciseActivity.java
+++ app/src/main/java/com/baertiger_baer/gymcoachbob/NewExerciseActivity.java
@@ -8,7 +8,6 @@ package com.baertiger_baer.gymcoachbob;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -20,14 +19,6 @@ public class NewExerciseActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_exercise);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return super.onOptionsItemSelected(item);
     }
 
     /** Callback for Radio Button presses. */
