73f1057bd3e6cdc45ccd0e4da72268e801133fb5
David Martin <david_martin@fastmail.com>
2017-03-05 23:31:25 +1100

    Remove unused import.

--- app/src/main/java/com/baertiger_baer/gymcoachbob/NewExerciseActivity.java
+++ app/src/main/java/com/baertiger_baer/gymcoachbob/NewExerciseActivity.java
@@ -8,7 +8,6 @@ package com.baertiger_baer.gymcoachbob;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
